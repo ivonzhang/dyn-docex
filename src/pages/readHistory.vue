451c22541a337f186d2a1fbe512dd2e3a339bbc0
ivonzhang-kingdee <zhangivon@gmail.com>
2017-05-04 14:07:47 +0800

    使用axios获取已读记录列表

--- src/pages/readHistory.vue
+++ src/pages/readHistory.vue
@@ -2,23 +2,20 @@
 	<div>
 		<div class="aui-tab" style="position: fixed; top: 0;z-index: 12;">
 			    <ul class="aui-tab-nav">
-			        <li class="tab_read actived">已读（1）</li>
+			        <li class="tab_read actived">已读（{{count}}）</li>
 			        <li class="tab_unread">未读（0）</li>
 			    </ul>
 			</div>
 			<div style="height:45px;width:100%"></div>
 			<div id="wrapper" ref="readWrapper">
 			    <ul>
-				   <li class="aui-list-view-cell" v-for="i in sum">
+				   <li class="aui-list-view-cell" v-for="user in results">
 				       <img class="aui-col-xs-2" :src="userImgSrc" style="width: 3.0rem; height: 3.0rem; border-radius: 50%;">
 				           <div class="aui-col-xs-7">
-				               <h4 style="height: 3.0rem;line-height: 3.0rem;margin-left: 0.5rem;">
-				                   张发
-				               </h4>
+				               <h4 style="height: 3.0rem;line-height: 3.0rem;margin-left: 0.5rem;" v-text="user.name"></h4>
 				           </div>
 				           <div class="aui-col-xs-3">
-				               <p class="aui-text-right font_title_other1" style="height: 3.0rem;line-height: 3.0rem;">
-				                   2017-01-19 10:23
+				               <p class="aui-text-right font_title_other1" style="height: 3.0rem;line-height: 3.0rem;" v-text="user.readdate">
 				               </p>
 				           </div>
 				       </img>
@@ -35,44 +32,70 @@
 	// import Vue from 'vue'
 	import axios from 'axios'
 	// Vue.prototype.$http = axios
+	import qs from 'qs'
 
 
 	export default{
 
 		data () {
 			return {
-				sum: 10
+				uid: 2,
+				docexId: 0,
+				pageSize: 12,
+				pageStart: 0,
+				type: 1,
+				results: []
 			}
 		},
 
 		computed: {
 			/*用户头像的url*/
 			userImgSrc(){
-				return api.serverInfo.domain + '/plugins/dyn/dyn.do?method=file&className=cn.firstsoft.firstframe.plugins.dyn.docex.service.DocexService&methodName=loadPhoto&params=3';
+				return `${api.serverInfo.domain}/plugins/dyn/dyn.do?method=file&className=cn.firstsoft.firstframe.plugins.dyn.docex.service.DocexService&methodName=loadPhoto&params=${this.uid}`;
 			},
+
+			count(){
+				return this.results.length
+			}
 		},
 
 		created() {
-
-
+			const vm = this
+			this.docexId = this.$route.query.docexId
+			this.uid = this.$route.query.sender
+			const params = [this.uid, this.docexId, this.pageStart, this.pageSize, this.type].join(",")
+			
+			/**
+			 * [myAxios description]默认情况下axios serializes JavaScript objects to JSON，使用application/x-www-form-urlencoded来提交数据的时候，
+			 * In a browser, you can use the URLSearchParams to format,或者使用
+			 * var qs = require('qs')这个库来format
+			 * @type {[type]}
+			 */
 			var myAxios = axios.create({
-			  baseURL: `${api.serverInfo.domain}/plugins/dyn/dyn.do?timestamp=1493361646241`,
+			  baseURL: `${api.serverInfo.domain}/plugins/dyn/dyn.do`,
 			  headers:{
 		        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
 		    },
-			  data:{
-	          method:'ajax',
-	          lastName:'cn.firstsoft.firstframe.plugins.dyn.docex.service.DocexService',
-	          methodName: 'getDocexFileReaderListByPageToKOA',
-	      },
-	      withCredentials: true
+		    params: {
+          timestamp: Date.parse(new Date()) / 1000
+        },
+			  data: {},
+	      // withCredentials: true //设置跨域请求,后台设置了"Access-Control-Allow-Origin","*"就不用开启了
 			})
 
-			myAxios.post('', {
-		    params: '3,15,0,12,0'
-		  })
+			myAxios.post('', qs.stringify({
+        method:'ajax',
+        className:'cn.firstsoft.firstframe.plugins.dyn.docex.service.DocexService',
+        methodName: 'getDocexFileReaderListByPageToKOA',
+		    params
+		  }))
 		  .then(function (response) {
 		    console.log(response);
+		    if(response.data.success === 1) {
+		    	vm.results = response.data.result.docexFileReaderVOs
+		    }else{
+		    	alert("获取阅读记录失败")
+		    }
 		  })
 		  .catch(function (error) {
 		    console.log(error);
