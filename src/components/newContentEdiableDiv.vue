537b9309933f78b12151996c47b671397d1e0dd4
ivonzhang-kingdee <zhangivon@gmail.com>
2017-05-10 11:01:27 +0800

    添加转发和新建协同工作时 正文内容的编辑框

---
+++ src/components/newContentEdiableDiv.vue
@@ -0,0 +1,43 @@
+<template>
+	<!--<div class="content_div"></div>-->
+	<div id="content" contenteditable="true" v-text="comment"  @input="changeText()"></div>
+	
+</template>
+
+<script type="text/javascript">
+
+	export default {
+
+		props:['value'],
+
+		data() {
+			return{
+				comment: this.value
+			}
+		},
+		methods: {
+			changeText(){
+				this.comment = this.$el.innerText;
+				console.log(this.comment)
+                this.$emit('input',this.comment);
+			}
+		}
+	}
+</script>
+
+<style type="text/css" scoped="">
+	div.content_div {
+	    background-color: #fff;
+	    margin: 0.5rem 0;
+	    z-index: 9;
+	}
+	div#content {
+	    max-height: 16rem;
+	    height: 16rem;
+	    outline: none;
+	    overflow-x: hidden;
+	    overflow-y: auto;
+	    margin: 5px;
+	    -webkit-user-select: text;
+	}
+</style>
